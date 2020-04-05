261de63889df534eb3a4ad8e7e6144849c0e5d26
Darren Zhang <darrenzhang200027@gmail.com>
2020-04-05 18:06:44 -0400

    added resolve function

--- server/app.js
+++ server/app.js
@@ -2,12 +2,18 @@
 const express = require('express')
 //this is like a supercharged endpoint where you send all of your queries
 const graphqlHTTP = require('express-graphql') 
+const schema = require('./schema/schema')
 
 const app = express()
 
-app.use('./graphql', graphqlHTTP)
+//when you make a request to /graphql, you want to tell the server to use the 
+//graphqlHTTP object which knows how to handle gql requests
+app.use('./graphql', graphqlHTTP({ //schemas are needed so you know what data you want back
+    schema
+}))
 
 app.listen(4000, ()=>{
     console.log('listening at port 4000')
 })
+
 
--- server/schema/schema.js
+++ server/schema/schema.js
@@ -1,26 +1,22 @@
-const graphql = require('graphql')
+const graphql = require("graphql")
+const_ = require("lodash")
 
-const {GraphQLObjectType, GraphQLString} = graphql
+const { GraphQLObjectType, GraphQLString, GraphQLSchema } = graphql
+
+// dummy data
+var books = [
+    { name: "Name of the Wind", genre: "Fantasy", id: "1" },
+    { name: "The Final Empire", genre: "Fantasy", id: "2" },
+    { name: "The Long Earth", genre: "Sci-Fi", id: "3" },
+]
 
 const BookType = new GraphQLObjectType({
     name: "Book",
-    field = () => ({
-        id: { type: GraphQLString},
-        id: { type: GraphQLString},
-        id: { type: GraphQLString}
-    })
-})
-
-//the entry points
-const RootQuery = new GraphQLObjectType({
-    name: 'RootQueryType',
-    field: {
-        type: BookType,
-        args: {id: { type: GraphQLString} }, 
-        resolve(parent, args){
-            //get data from db
-        }
-    }
+    fields: () => ({
+        id: { type: GraphQLString },
+        id: { type: GraphQLString },
+        id: { type: GraphQLString },
+    }),
 })
 
 /*
@@ -30,3 +26,21 @@ book(id: '123){
     genre
 }
 */
+//the entry points
+const RootQuery = new GraphQLObjectType({
+    name: "RootQueryType",
+    fields: {
+        book: {
+            type: BookType,
+            args: { id: { type: GraphQLString } },
+            resolve(parent, args) {
+                //get data from db
+                return _.find(book, { id: args.id })
+            },
+        },
+    },
+})
+
+module.exports = new GraphQLSchema({
+    query: RootQuery,
+})
