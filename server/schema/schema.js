ebd64761bb878cb53a7c3624beee178727c26201
Darren Zhang <darrenzhang200027@gmail.com>
2020-04-05 16:32:35 -0400

    added rootquery

--- server/schema/schema.js
+++ server/schema/schema.js
@@ -10,3 +10,23 @@ const BookType = new GraphQLObjectType({
         id: { type: GraphQLString}
     })
 })
+
+//the entry points
+const RootQuery = new GraphQLObjectType({
+    name: 'RootQueryType',
+    field: {
+        type: BookType,
+        args: {id: { type: GraphQLString} }, 
+        resolve(parent, args){
+            //get data from db
+        }
+    }
+})
+
+/*
+In the frontend, you do something like:
+book(id: '123){
+    name
+    genre
+}
+*/
