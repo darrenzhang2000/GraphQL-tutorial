4ac5e0a1eed1dc74a8f29424fad234f7a5f199a4
Darren Zhang <darrenzhang200027@gmail.com>
2020-04-05 16:13:20 -0400

    added book schema

--- server/app.js
+++ server/app.js
@@ -1,7 +1,13 @@
+// need to npm i graphql and express-graphql
 const express = require('express')
+//this is like a supercharged endpoint where you send all of your queries
+const graphqlHTTP = require('express-graphql') 
 
 const app = express()
+
+app.use('./graphql', graphqlHTTP)
 
 app.listen(4000, ()=>{
     console.log('listening at port 4000')
 })
+
